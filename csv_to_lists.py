497f6f6ccc304304792e62f026f5586d8805c7e1
eobak1 <62924978+eobak1@users.noreply.github.com>
2021-05-05 13:52:31 +1000

    csv reader
    added csv to lists.

---
+++ csv_to_lists.py
@@ -0,0 +1,19 @@
+import csv
+cp = []
+all=[]
+with open('carpark3.csv') as csvfile:
+    rows = csv.reader(csvfile)
+    res = list(zip(*rows))
+r = len(res)
+for i in range((r-2)-1):
+    i = i + 2
+    if (i % 2==0):
+        cpnum= res[i][0][14:]
+        lots= res[i+1][2]
+        avail = res[i][1:]
+        time = res[1][1:]
+        cp=[cpnum, lots, avail,time]
+        all.append(cp)
+
+#print(res[3][0:5])
+print(all[7])
